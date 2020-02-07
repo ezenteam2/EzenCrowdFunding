28ee5d6e1680a488d824af5033ada53a138fe3d7
dlgudwnsgpffh <59990737+dlgudwnsgpffh@users.noreply.github.com>
2020-02-07 16:31:12 +0900

    변경

--- gr2_lhj/page/public/user_w_hj_make_reward.js
+++ gr2_lhj/page/public/user_w_hj_make_reward.js
@@ -2,6 +2,9 @@ const titleLengthIndicator = document.querySelector('.title_length_indicator');
 const projectTitle = document.getElementById("project_title");
 const rightPriceIndicator = document.querySelector('.right_price_indicator');
 const projectTargetFund = document.getElementById('project_target_fund');
+const dropBox=document.querySelector('.image_drop_box');
+
+console.log(dropBox);
 
 function handleTitleLength(event){
     const titleLength =event.target.value.length;
@@ -10,7 +13,6 @@ function handleTitleLength(event){
 
 function handleTargetFund(event){
     const targetFund = event.target.value;
-    console.log();
     if(!Number.isNaN(Number(targetFund))){
         if(Number(targetFund)>=100000){
             rightPriceIndicator.innerHTML='';
@@ -22,5 +24,17 @@ function handleTargetFund(event){
     }
 }
 
+function onDragOver(event){
+    event.preventDefault();
+}
+
+function onDrop(event){
+    event.preventDefault();
+    console.log(event);
+}
+
 projectTitle.addEventListener("keyup", handleTitleLength);
 projectTargetFund.addEventListener("keyup", handleTargetFund);
+
+dropBox.addEventListener('dragover', onDragOver);
+dropBox.addEventListener('drop', onDrop);
