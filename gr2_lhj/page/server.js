b3ea89c346d8feed658cde90602486b70d99d2b0
이형준 <hjplayer01@naver.com>
2020-02-10 00:08:02 +0900

    수정

--- gr2_lhj/page/server.js
+++ gr2_lhj/page/server.js
@@ -7,7 +7,7 @@ app.get('/',function(req,res){
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
-app.use(express.static('public'));
+app.use(express.static('views'));
 
 var server = app.listen(3000, function(){
     console.log("Express server has started on port 3000")
