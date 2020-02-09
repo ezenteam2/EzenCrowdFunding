24eee22f8659864bc4939f99f8192833c3f462f0
yuseohee27 <59616102+yuseohee27@users.noreply.github.com>
2020-02-09 19:18:13 +0900

    Update login.js

--- gr2_ysh/admin/js/login.js
+++ gr2_ysh/admin/js/login.js
@@ -1,6 +1,7 @@
 var redirectTime = "1500";
 var redirectURL = "./index.html";
 function login_process() {
+        history.replaceState({}, null, location.pathname);
         document.querySelector('.form').style.display='none';
         document.querySelector('.title').style.transition='2s';
         document.querySelector('.title').style.transform='translateY(110px)';    
