b2e889adc57e6e88df4472e18a1b2c8d157733e2
dlgudwnsgpffh <59990737+dlgudwnsgpffh@users.noreply.github.com>
2020-02-11 18:14:41 +0900

    수정

--- gr2_lhj/page/views/insert-detail-image.js
+++ gr2_lhj/page/views/insert-detail-image.js
@@ -1,23 +1,56 @@
 {
 const imageWrap = document.querySelector('.image_wrap');
 const detailImgInput=document.getElementById('detail_img_input');
+const initialCloseIcon=document.querySelector('.detail_img_delete .close_icon');
+let deleteDivs = [document.querySelector('.detail_img_delete')];
 let detailImg = [document.querySelector('.detail_img')];
-let imgCnt=0;
+let Cnt=0;
 
-console.log(detailImg);
+function clickDeleteBtn(event){
+    const _imgBox=event.target.parentNode.parentNode;
+    imageWrap.removeChild(_imgBox);
+}
+
+function createDeleteDiv(){
+    const deleteDiv=document.createElement('div');
+    const closeIcon=document.createElement('img');
+    const fileName=document.createElement('div');
+
+    closeIcon.addEventListener('click', clickDeleteBtn);
+    closeIcon.src='./cross.png';
+    closeIcon.width='30';
+    closeIcon.height='30';
+
+    closeIcon.onclick=clickDeleteBtn;
+    closeIcon.classList.add('close_icon')
+    deleteDiv.classList.add('detail_img_delete');
+    deleteDiv.classList.add('none');
+    fileName.classList.add('detail_img_file_name');
+
+    deleteDiv.appendChild(closeIcon);
+    deleteDiv.appendChild(fileName);
+    deleteDiv.addEventListener('mouseenter', handleMouseIn);
+    deleteDiv.addEventListener('mouseleave', handleMouseOut);
+    deleteDivs.push(deleteDiv);
+
+    return deleteDiv;
+}
 
 function createImageBox(){
     const imageBox=document.createElement('div');
     const inputFile=document.createElement('input');
     const newImg=document.createElement('img');
+    const deleteDiv=createDeleteDiv();
+
     imageBox.classList.add('image_box');
     inputFile.type='file';
     inputFile.accept='image/*';
     inputFile.addEventListener('change', handleDetailFiles);
     detailImg.push(newImg);
+    Cnt++;
     imageBox.appendChild(newImg);
     imageBox.appendChild(inputFile);
-
+    imageBox.appendChild(deleteDiv);
     return imageBox;
 }
 
@@ -25,26 +58,42 @@ function insertImageBox(){
     imageWrap.appendChild(createImageBox());
 }
 
-function handleDetailFiles(event){
-    console.log(event);
-    previewFile(event.target.files[0]);
-    detailImg[0].classList.remove('none');
-    insertImageBox();
+function handleMouseIn(event){
+    event.target.style.opacity=1;
 }
 
-function previewFile(file){
+function handleMouseOut(event){
+    event.target.style.opacity=0;
+}
+
+
+function handleDetailFiles(event){
+    previewFile(event.target.files[0], insertImageBox);
+    event.target.disabled=true;
+}
+
+function previewFile(file, callback){
     const reader = new FileReader();
     reader.readAsDataURL(file);
     if(file.type==='image/jpeg'||file.type==='image/png'){
         reader.onloadend=function(){
-            detailImg[0].src = reader.result;
-            detailImg.width='200';
-            detailImg.height='200';
+        detailImg[Cnt].src = reader.result;
+        detailImg[Cnt].width='200';
+        detailImg[Cnt].height='200';
+        detailImg[Cnt].classList.remove('none');
+        deleteDivs[Cnt].classList.remove('none');
+        deleteDivs[Cnt].lastElementChild.innerHTML=file.name;
+        callback();
+        console.log(detailImg);
     } 
     } else{
     alert("그림파일은 jpg, png형식만 가능합니다.")
     }
 }
+
+deleteDivs[0].addEventListener('mouseenter', handleMouseIn);
+deleteDivs[0].addEventListener('mouseleave', handleMouseOut);
 detailImgInput.addEventListener('change', handleDetailFiles);
+initialCloseIcon.addEventListener('click', clickDeleteBtn);
 
 }
