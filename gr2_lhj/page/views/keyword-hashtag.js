57aa89f9b5f7a574b105ebb8d4908238b5e28d22
이형준 <hjplayer01@naver.com>
2020-02-13 23:39:08 +0900

    수정 git commit -a -m 수정 git commit -a -m 수정

--- gr2_lhj/page/views/keyword-hashtag.js
+++ gr2_lhj/page/views/keyword-hashtag.js
@@ -1,14 +1,33 @@
 {
 const keywordTextInput=document.getElementById('project_key_words');
 const keywordBar=document.querySelector('.keyword_bar');
+const dataArray=[];
 
 
 function focusToTextBar(event){
     keywordTextInput.focus();
 }
+
+
+function getIndex(element){
+    const parent=element.parentNode;
+    let index=0;
+    for (let child of parent.children){
+        if(element===child){
+            break;
+        } else{
+            index++;
+        }
+    }
+    console.log(index);
+    return index;
+}
 function clickDelete(event){
+    const index=getIndex(event.target.parentNode);
+    dataArray.splice(index,1);
     const targetElement=event.target.parentNode;
     keywordBar.removeChild(targetElement);
+    console.log(dataArray);
 }
 
 function handleKeyUp(event){
@@ -17,6 +36,8 @@ function handleKeyUp(event){
         const deleteComma=keyword.replace(',','');
         if(deleteComma!==''){
             keywordTextInput.value='';
+            dataArray.push(deleteComma);
+            console.log(dataArray);
             const element=document.createElement('span');
             const innerElement=document.createElement('span');
             element.innerText=deleteComma;
