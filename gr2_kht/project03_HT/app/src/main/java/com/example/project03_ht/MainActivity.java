b8e21beb28aa7f856a8fa182c614696e3967fcc2
esseone7 <59997115+esseone7@users.noreply.github.com>
2020-02-20 01:36:43 +0900

    asd

--- gr2_kht/project03_HT/app/src/main/java/com/example/project03_ht/MainActivity.java
+++ gr2_kht/project03_HT/app/src/main/java/com/example/project03_ht/MainActivity.java
@@ -1,28 +1,47 @@
 package com.example.project03_ht;
 
+
         import androidx.appcompat.app.AppCompatActivity;
 
         import android.content.Intent;
         import android.os.Bundle;
         import android.view.View;
+
         import android.widget.Button;
+        import android.widget.ImageView;
+        import android.widget.TextView;
         import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView intent_textview;
+    ImageView intent_imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.user_m_ht_category_menu);
 
-        Button recommend = (Button)findViewById(R.id.recommend);
-        recommend.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent myintent = new Intent(MainActivity.this,recommend.class);
-                startActivity(myintent);
-                finish();
+        intent_textview = findViewById(R.id.textView);
 
+        intent_textview.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent01 = new Intent(MainActivity.this, SubActivity.class);
+                startActivity(intent01);
+            }
+        });
+    }
+
+    protected void onCreate02(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_m_ht_projects_detail);
+
+        intent_imageView = findViewById(R.id.imageView6);
+
+        intent_imageView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent02 = new Intent(MainActivity.this, SubActivity02.class);
+                startActivity(intent02);
             }
         });
     }
---
+++ gr2_kht/project03_HT/app/src/main/java/com/example/project03_ht/SubActivity.java
@@ -0,0 +1,17 @@
+package com.example.project03_ht;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.Toast;
+
+public class SubActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle SavedInstanceState) {
+        super.onCreate(SavedInstanceState);
+        setContentView(R.layout.user_m_ht_projects_list);
+    }
+}
