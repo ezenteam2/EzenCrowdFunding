6c79d6b5c9ad9fb97946630fa9bf9f98f8f0874d
yuseohee27 <59616102+yuseohee27@users.noreply.github.com>
2020-02-20 15:23:23 +0900

    Update mypage.java

--- EZENFUNDING/app/src/main/java/com/example/ezenfunding/mypage.java
+++ EZENFUNDING/app/src/main/java/com/example/ezenfunding/mypage.java
@@ -2,7 +2,11 @@ package com.example.ezenfunding;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class mypage extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class mypage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mypage);
+        Button button5 = (Button)findViewById(R.id.button5);
+
+        button5.setOnClickListener(new Button.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent intentbutton5 = new Intent(mypage.this,Page1.class);
+                startActivity(intentbutton5);
+            }
+        });
     }
 }
