001a99256d4272c868528b45a3188abf480d47ec
esseone7 <59997115+esseone7@users.noreply.github.com>
2020-02-20 16:59:30 +0900

    asdf

--- gr2_kht/WebContent/gr2_ysh/admin/js/login.js
+++
@@ -1,8 +0,0 @@
-var redirectTime = "1500";
-var redirectURL = "./index.html";
-function login_process() {
-        document.querySelector('.form').style.display='none';
-        document.querySelector('.title').style.transition='2s';
-        document.querySelector('.title').style.transform='translateY(110px)';    
-        setTimeout("location.href = redirectURL;",redirectTime);
-}    
