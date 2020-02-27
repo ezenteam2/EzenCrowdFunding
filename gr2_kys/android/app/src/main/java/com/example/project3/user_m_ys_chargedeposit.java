97bf8b94c419fbfa1571ef7884d2cd9206a62d45
perpreme <60067300+perpreme@users.noreply.github.com>
2020-02-27 16:35:01 +0900

    아몰라끝이다~~

---
+++ gr2_kys/android/app/src/main/java/com/example/project3/user_m_ys_chargedeposit.java
@@ -0,0 +1,12 @@
+package com.example.project3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class user_m_ys_chargedeposit extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_m_ys_chargedeposit);
+    }
+}
